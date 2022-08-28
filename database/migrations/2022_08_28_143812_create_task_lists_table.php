47dd90dc6b5504b8147d9077c01a7dee791c6b1e
Rajib-hossain-cse-rmstu <rajibraju771@gmail.com>
2022-08-28 20:44:26 +0600

    Create Model, Controller and Migration

---
+++ database/migrations/2022_08_28_143812_create_task_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('task');
+            $table->enum('process', ['inprogress', 'waiting', 'done'])->default('waiting');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_lists');
+    }
+};
